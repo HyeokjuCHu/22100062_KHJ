bcfb4122c47b5953b654182caae1c953fb6e8272
HyeokjuCHu <22100062@handong.ac.kr>
2024-04-05 21:46:22 +0900

    Create J002

--- src/week5/Main.java
+++ src/week5/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Main pStudio =new Main();
-        pStudio.J001();
+        pStudio.J002();
     }
 
     void J001() {
@@ -17,5 +17,12 @@ public class Main {
         System.out.printf("%.1f",bmi);
     }
 
+    void J002() {
+        Scanner in = new Scanner(System.in);
+        double c_degree = in.nextDouble();
+        double f_degree = c_degree *1.8 + 32;
+        System.out.printf("%.1f",f_degree);
+
+    }
 
 }
