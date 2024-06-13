2ef161b31956d653f461addcfdee8ef8820d792c
HyeokjuCHu <22100062@handong.ac.kr>
2024-06-13 20:39:22 +0900

    Create final

---
+++ src/javafinal/Account.java
@@ -0,0 +1,12 @@
+package javafinal;
+
+import java.util.List;
+
+public interface Account {
+    String getAccountNumber();
+    String getOwner();
+    int getBalance();
+    void deposit(int amount);
+    int withdraw(int amount) throws Exception;
+    List<String> getTransactionHistory();
+}
